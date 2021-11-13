ccd943650723e4efdd822ddc7f2e9289aaa9c457
Nikolai Berestevich (Mikalai Berastsevich) <nikolai.berestevich@gmail.com>
2021-11-13 19:22:12 +0300

    feat: add myWriteStream

---
+++ src/streams/myWriteStream.js
@@ -0,0 +1,38 @@
+
+const { Writable } = require('stream')
+const fs = require('fs')
+
+class MyWriteStream extends Writable {
+    constructor(filename) {
+        super()
+        this.filename = filename
+        this.fd = null
+    }
+
+    _construct(callback) {
+        fs.open(this.filename, 'a+', (err, fd) => {
+            if (err) {
+                throw new Error(`application can't open ${this.filename} `)
+            } else {
+                this.fd = fd
+                callback()
+            }
+        })
+    }
+
+    _write(chunk, _encoding, callback) {
+        fs.write(this.fd, chunk, callback)
+    }
+
+
+    _destroy(err, callback) {
+        if (this.fd) {
+            fs.write(this.fd, '', callback)
+            fs.close(this.fd, (er) => callback(er || err))
+        } else {
+            throw new Error(`application can't write to ${this.filename} `)
+        }
+    }
+}
+
+module.exports = MyWriteStream
